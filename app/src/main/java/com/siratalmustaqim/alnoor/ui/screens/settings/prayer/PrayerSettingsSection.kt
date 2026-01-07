2ced10f8072e34d400b16d695684ff216ad5c3aa
Md Sadique Inam <md.sadique32@gmail.com>
2026-01-07 08:10:37 +0530

    refactor: update hiltViewModel import path to androidx.hilt.lifecycle.viewmodel.compose

--- app/src/main/java/com/siratalmustaqim/alnoor/ui/screens/settings/prayer/PrayerSettingsSection.kt
+++ app/src/main/java/com/siratalmustaqim/alnoor/ui/screens/settings/prayer/PrayerSettingsSection.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.siratalmustaqim.alnoor.ui.components.SettingsRowDropdown
 import com.siratalmustaqim.alnoor.ui.components.SettingsRowText
 import com.siratalmustaqim.alnoor.ui.components.SettingsSection
